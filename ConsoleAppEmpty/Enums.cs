b5f8c04c23fb12f75b672dee54e5c7aa8419ae93
Stockton.Stephens <Stocktonschool02@gmail.com>
2026-01-13 14:30:04 -0700

    added part 1

---
+++ ConsoleAppEmpty/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameEnums
+{
+    enum GameDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    enum GameClass
+    {
+        Warrior, 
+        Mage, 
+        Rogue
+    }
+}
